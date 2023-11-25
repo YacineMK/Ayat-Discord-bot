5257ea50b507ba3aaa9c9d9ed8ab81fa004a0aca
YacineMK <merroukiyacine06@gmail.com>
2023-11-25 17:38:35 +0100

    add token2.0

--- main.go
+++ main.go
@@ -9,7 +9,8 @@ func main () {
 	token := "test" 
 	dg , err := discordgo.New("BOT" + token) 
 	if err != nil {
-		fmt.Println("err : %v", err);
+		fmt.Println("err : %v", err)
+		return
 	}
 	fmt.Println("Bot is now running. Press Ctrl+C to exit.");
 }
